3868ae6c99a07cedd8b3c043c67bb171c4fe851f
Alexandre <160348214+NeosRuby@users.noreply.github.com>
2024-06-10 18:09:08 -0300

    Add files via upload

---
+++ challenge literalura/literalura/src/main/java/br/com/neosruby/literalura/repository/LivroRepository.java
@@ -0,0 +1,17 @@
+package br.com.neosruby.literalura.repository;
+
+import br.com.neosruby.literalura.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    Livro findByTitulo(String titulo);
+
+    @Query("SELECT l FROM Livro l WHERE l.idioma LIKE %:idioma%")
+    List<Livro> findLivrosPorIdioma(String idioma);
+
+}
